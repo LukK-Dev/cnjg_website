e5dd71807bb1cb2325f55be09b018658848819f4
Luk <luk@kaiser-gotha.de>
2021-12-19 21:24:39 +0100

    some js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 fetch('https://api.chucknorris.io/jokes/random')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => data)
+
+const api_url = 'https://api.chucknorris.io/jokes/random'
+const generate_btn = document.getElementById('generate-btn')
+const text_container = document.getElementById('text-container')
+
+generate_btn.addEventListener('click', e => {
+    text_container.innerText = 'hello'
+})
